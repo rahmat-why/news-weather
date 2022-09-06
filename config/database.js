f54f6a18f05129e9f8edef3ecf45daa4c07021be
rahmat_why <rahmatwahyubudiyanto@gmail.com>
2022-09-06 21:07:24 +0700

    typo

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 import { Sequelize, QueryTypes } from'sequelize'
-const database = new Sequelize(PROCESS.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+const database = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
   
