e5470d05c2148d77505c289302ef2ddc5b42119c
rahmat_why <rahmatwahyubudiyanto@gmail.com>
2022-09-06 20:42:37 +0700

    env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 import { Sequelize, QueryTypes } from'sequelize'
-const database = new Sequelize('bot_wa_news_weather', 'root', '', {
+const database = new Sequelize(PROCESS.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
   
