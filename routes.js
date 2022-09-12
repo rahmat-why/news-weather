d6b6b29c62f10eee830668d9d86d8d85cc9d2685
root <rahmatwahyubudiyanto@gmail.com>
2022-09-12 20:08:05 +0700

    fix bug cron

--- routes.js
+++ routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import response from "./response.js";
 import * as WhatsappController from "./controllers/WhatsappController.js";
+import * as EtlWeatherCOntroller from "./controllers/EtlWeatherCOntroller.js";
+import notifySubscribers from "./functions/notifySubscribers.js";
 
 const router = Router();
 
 router.post("/webhook", WhatsappController.webhook);
+router.post("/etl-weather", EtlWeatherCOntroller.etlWeather);
+router.post("/notify-subscriber", notifySubscribers);
 
 router.all("*", (req, res) => {
   response(res, 404, false, "The requested url cannot be found.");
